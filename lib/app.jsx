ea927f54d357e79523291089e76691714d308172
pastekfrite <elsamothay@hotmail.com>
2015-11-17 15:54:48 +0100

    6

--- lib/app.jsx
+++ lib/app.jsx
@@ -8,27 +8,26 @@ var boxStyle = {
     'background-color':'blue'
 };
 
+var timerValue=300;
+
+
 var Box = React.createClass({
-    componentWillMount: function(){
-        var old_this = this;
-        this.timer = setInterval(function(){
-        var oldValue = old_this.state.value;
+
+    handleClick: function(){
+        var oldValue = this.state.value;
         var newValue = oldValue === 'X' ? 'O': 'X';
-        old_this.setState({
+        this.setState({
         value: newValue
         });
-        }, 300);
     },
-    componentWillUnmount: function(){
-        clearInterval(this.timer);
-    },
+
     getInitialState: function() {
         return {value: this.props.initialValue};
     },
     'render': function onRender () {
     return (
         <div>
-            <button style={boxStyle}>{this.state.value}</button>
+            <button style={boxStyle} onClick={this.handleClick}>{this.state.value}</button>
         </div>
     );
    }
